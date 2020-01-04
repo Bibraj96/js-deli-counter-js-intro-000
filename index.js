288b3a6df5e22cf68ac84b26adeead33f4b4cb04
Blendi Ibraj <blendi.ibraj96@gmail.com>
2020-01-04 17:34:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-function takeANumber(katzDeliLine,name){
+function takeANumber(katzDeliLine,name) {
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  if (katsDeliLine.length > 0) return `Currently serving ${katsDeliLine.shift().}`;
+  else return "There is nobody waiting to be served!";
 }
